333324aa88dfd600ba631ae79a619692ae73002c
codechenx <codechenx@gmail.com>
2024-02-10 19:06:28 +0100

    fix bug
    	new file:   .gitignore
    	modified:   bin/dotfiles/main.sh
    	deleted:    config/.DS_Store
    	modified:   config/bash/aliases.bash
    	modified:   config/bash/functions.bash
    	modified:   config/fish/config.fish
    	modified:   config/fish/export.fish
    	deleted:    config/fish/functions/c_mtr.fish
    	deleted:    config/fish/functions/flushdns.fish
    	modified:   config/fish/functions/pupdate.fish
    	modified:   config/fish/functions/supdate.fish
    	deleted:    config/fish/functions/treload.fish
    	modified:   config/zsh/.zshrc
    	modified:   system/macos/setup_packages.sh
    	modified:   system/macos/setup_shell.sh
    	modified:   system/pop/setup_packages.sh
    	modified:   system/pop/setup_shell.sh

--- config/bash/functions.bash
+++ config/bash/functions.bash
@@ -1,25 +1,3 @@
-# GPG Public Key
-function gpgpubkey() {
-    gpg --armor --export "$@" | pbcopy | echo 'GPG Public Key copied to pasteboard.'
-}
-
-# Flush DNS
-function flushdns() {
-    printf "\nFlushing DNS\n"
-    OS=$(uname)
-    case $OS in
-    Linux)
-        sudo systemd-resolve --flush-caches
-        ;;
-    Darwin)
-        dscacheutil -flushcache
-        ;;
-    esac
-
-    tmux display-message "Flushed DNS"
-}
-
-
 # Fast CD into directories
 function fcd() {
     cd $(tree -dfia -L 1 | fzf) || exit
@@ -45,16 +23,6 @@ function c_prettyping() {
     fi
 }
 
-# Custom Pretty Ping
-function c_mtr() {
-
-    if [ $# -eq 0 ]; then
-        sudo mtr 1.1.1.1
-    else
-        sudo mtr "$@"
-    fi
-}
-
 function pupdate() {
     printf "\nUpdating starfish\n"
     OS=$(uname)
@@ -68,12 +36,6 @@ function pupdate() {
     esac
 
     tmux display-message "Starship Prompt Update Completed"
-}
-
-function treload ()
-{
-    tmux source-file ~/.config/tmux/tmux.conf
-    tmux display-message "TMUX Config Reloaded"
 }
 
 function ql()
@@ -91,17 +53,8 @@ function ql()
 
 if [ "$0" = "bash" ]
 then
-    export -f gpgpubkey
-    export -f flushdns
     export -f mkcd
     export -f code
     export -f c_prettyping
-    export -f c_mtr
     export -f pupdate
-fi
-
-# Add HDD aliases for WSL cd into mounted drives
-if [[ $(grep -i Microsoft /proc/version) ]]; then
-    alias C="cd /mnt/c"
-    alias D="cd /mnt/d"
 fi
