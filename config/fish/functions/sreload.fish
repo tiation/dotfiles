9bd5a58b48906fcb9fb9e4c00d9a4e716683b053
codechenx <codechenx@gmail.com>
2024-02-10 19:06:28 +0100

    fix bug
    	new file:   .gitignore
    	modified:   bin/dotfiles/main.sh
    	deleted:    config/.DS_Store
    	modified:   config/bash/aliases.bash
    	modified:   config/bash/functions.bash
    	modified:   config/fish/config.fish
    	modified:   config/fish/export.fish
    	deleted:    config/fish/functions/c_mtr.fish
    	deleted:    config/fish/functions/flushdns.fish
    	modified:   config/fish/functions/pupdate.fish
    	modified:   config/fish/functions/supdate.fish
    	deleted:    config/fish/functions/treload.fish
    	modified:   config/zsh/.zshrc
    	modified:   system/macos/setup_packages.sh
    	modified:   system/macos/setup_shell.sh
    	modified:   system/pop/setup_packages.sh
    	modified:   system/pop/setup_shell.sh
    
    	modified:   bin/dotfiles/main.sh
    	modified:   config/bash/.bashrc
    	modified:   config/bash/aliases.bash
    	modified:   config/bash/functions.bash
    	modified:   system/macos/setup_packages.sh
    	modified:   system/macos/setup_shell.sh
    	modified:   system/pop/setup_packages.sh
    	modified:   system/pop/setup_shell.sh
    
    	modified:   config/bash/.bashrc
    	modified:   config/bash/aliases.bash
    	modified:   config/fish/export.fish
    	modified:   system/macos/setup_packages.sh
    	modified:   system/macos/setup_shell.sh
    	modified:   system/pop/setup_shell.sh
    
    	modified:   config/fish/export.fish
    	deleted:    config/tmux/tmux.conf
    	modified:   system/symlink.sh
    
    modified:   config/fish/export.fish
    deleted:   config/tmux/tmux.conf
    modified:   system/symlink.sh
    
    	modified:   config/fish/export.fish
    	modified:   config/fish/functions/sreload.fish

--- config/fish/functions/sreload.fish
+++ config/fish/functions/sreload.fish
@@ -2,5 +2,4 @@ function sreload --description 'Reloads fish configs'
     . ~/.config/fish/config.fish
     . ~/.config/fish/export.fish
     . ~/.config/fish/bindings.fish
-    tmux display-message "Fish Shell Config Reloaded"
 end
